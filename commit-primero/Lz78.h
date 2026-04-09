98c23bafd9995a37ca79a832a282ed9ef895f587
Juan Jose Ortega <juan.omesias@udea.edu.co>
2026-04-09 09:43:32 -0500

    Ultimo final con todos los cambios

--- commit-primero/Lz78.h
+++ commit-primero/Lz78.h
@@ -1,13 +1,13 @@
 #ifndef LZ78_H
 #define LZ78_H
 
-// Estructura que representa una entrada en el diccionario de compresión
+
 struct Entry {
-    int prefix; // Índice del prefijo anterior en el diccionario
-    char c;     // El nuevo carácter que se añade al prefijo
+    int prefix;
+    char c;
 };
 
-// Prototipos de las funciones para que puedan ser usadas en el .cpp
+
 int findEntry(Entry* dict, int size, int prefix, char c);
 void compressLZ78(const char* input, int indices[], char chars[], int &outSize);
 void decompressLZ78(int indices[], char chars[], int size);
--- commit-primero/RLE.h
+++ commit-primero/RLE.h
@@ -1,10 +1,9 @@
 #ifndef RLE_H
 #define RLE_H
 
-// Comprimir
+
 void rleCompress(const char* input, char* output);
 
-// Descomprimir
 void rleDecompress(const char* input, char* output);
 
 #endif
